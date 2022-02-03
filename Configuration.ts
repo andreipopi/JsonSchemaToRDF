304516ad9ae6a79b044574566ced11819da38f2f
Andrei Popescu <andrei-popescu@outlook.com>
2022-02-03 14:33:47 +0100

    main.js uploaded as well
    It needs to be there otherwise the pipeline will fail.

--- Configuration.ts
+++ Configuration.ts
@@ -16,6 +16,13 @@ export class Configuration {
         
         this.baseRdfVocabURI = 'https://w3id.org/gbfs/stations#';
     
+
+        this.map.set('bike_id', 'dcterms:identifier');
+        this.map.set('alert_id', 'dcterms:identifier');
+        this.map.set('station_id','dcterms:identifier');
+        this.map.set('vehicle_type_id','dcterms:identifier');
+
+
         this.map.set('description','dcterms:description');
         this.map.set('last_updated', 'dcterms:modified' );
         this.map.set('type', 'rdf:type');
@@ -23,7 +30,6 @@ export class Configuration {
         this.map.set('summary', 'ebucore:summary');
 
         // Station properties terms
-        this.map.set('station_id','dcterms:identifier');
         this.map.set( 'name', 'foaf:name');
         this.map.set( 'short_name', 'rdfs:label');
         this.map.set( 'lat', 'geo:lat');
@@ -33,11 +39,13 @@ export class Configuration {
         this.map.set( 'post_code', 'dbpedia-owl:postalCode');
         this.map.set( 'capacity', 'dbpedia-owl:capacity');
         // FreeBikeStatus properties terms
-        this.map.set('bike_id', 'dcterms:identifier');
-        this.map.set('alert_id', 'dcterms:identifier');
+        
         this.map.set('creditcard', 'schema:CreditCard');
         this.map.set('phone', 'foaf:phone');
         
+        // vehicle types
+
+
 
 
     };
---
+++ shaclShape.js
@@ -0,0 +1,101 @@
+"use strict";
+// We would need to define different shapes for different jsons
+exports.__esModule = true;
+exports.ShaclShape = void 0;
+var ShaclShape = /** @class */ (function () {
+    // Constructors
+    function ShaclShape(required, source, mainObj) {
+        this.shaclRoot = '<https://mymockwebsite.com/shapes/gbfs-station_information>';
+        this.requiredProperties = new Map();
+        this.shaclFileText = '';
+        this.fs = require('fs');
+        this.jsonSchema = require(source);
+        this.requiredProperties = required;
+        console.log("passed object", mainObj);
+        console.log("main target", this.getShaclTarget(mainObj));
+        this.targetClass = this.getShaclTarget(mainObj);
+    }
+    // Methods
+    ShaclShape.prototype.getShaclTypedProperty = function (nome, type) {
+        var prop = 'sh:property [ \n sh:path ' + nome + '; \n sh:maxCount 1; \n sh:datatype ' + type + '; \n ];';
+        return prop;
+    };
+    ShaclShape.prototype.getShaclTypedRequiredProperty = function (nome, type) {
+        console.log(nome);
+        var prop = 'sh:property [ \n sh:path ' + nome + ';  \n sh:minCount 1; \n sh:maxCount 1; \n sh:datatype ' + type + '; \n ];';
+        return prop;
+    };
+    ShaclShape.prototype.getShaclProperty = function (nome) {
+        var prop = 'sh:property [ \n sh:path ' + nome + '; \n sh:maxCount 1; \n ];';
+        return prop;
+    };
+    ShaclShape.prototype.getShaclRequiredProperty = function (nome) {
+        console.log(nome);
+        var prop = 'sh:property [ \n sh:path ' + nome + ';  \n sh:minCount 1; \n sh:maxCount 1; \n ];';
+        return prop;
+    };
+    ShaclShape.prototype.getShaclTargetClass = function () {
+        return 'sh:targetClass ' + this.targetClass + ';';
+    };
+    ShaclShape.prototype.getShaclTarget = function (mainObject) {
+        switch (mainObject) {
+            case 'gbfsvcb:Station': {
+                return '<https://w3id.org/gbfs/station>';
+                break;
+            }
+            case 'gbfsvcb:Bike': {
+                return '<https://w3id.org/gbfs/bike>';
+                break;
+            }
+            case 'gbfsvcb:Alert': {
+                return '<https://w3id.org/gbfs/alert>';
+                break;
+            }
+            case 'gbfsvcb:Region': {
+                return '<https://w3id.org/gbfs/region>';
+                break;
+            }
+            case 'gbfsvcb:VehicleType': {
+                return '<https://w3id.org/gbfs/vehicleType>';
+                break;
+            }
+            case 'gbfsvcb:PricingPlan': {
+                return '<https://w3id.org/gbfs/pricingPlan>';
+                break;
+            }
+            case 'gbfsvcb:Version': {
+                return '<https://w3id.org/gbfs/version>';
+                break;
+            }
+            case 'gbfsvcb:Calendar': {
+                return '<https://w3id.org/gbfs/calendar>';
+                break;
+            }
+            case 'gbfsvcb:RentalHour': {
+                return '<https://w3id.org/gbfs/rentalHour>';
+                break;
+            }
+            case 'gbfsvcb:Feed': {
+                return '<https://w3id.org/gbfs/feed>';
+                break;
+            }
+            default: {
+                //statements; 
+                break;
+            }
+        }
+    };
+    ShaclShape.prototype.getShaclRoot = function () {
+        return this.shaclFileText = this.shaclRoot + ' a sh:NodeShape; \n';
+    };
+    ShaclShape.prototype.isRequired = function (prop) {
+        if (this.requiredProperties.has(prop)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return ShaclShape;
+}());
+exports.ShaclShape = ShaclShape;
