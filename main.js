49451d2a2a67e98b0fb06b95965f5bdebed545dc
Andrei Popescu <andrei-popescu@outlook.com>
2022-02-16 15:10:11 +0100

    updated main: more understandable
    - useful in order to write more concise pseudocode algorithms in overleaf

--- main.js
+++ main.js
@@ -3,59 +3,72 @@ exports.__esModule = true;
 var Configuration_1 = require("./Configuration");
 var rdfVocabulary_1 = require("./rdfVocabulary");
 //import {ShaclShape} from './shaclShape';
-var files = ['./files/station_information.json', './files/free_bike_status.json', './files/system_alerts.json', './files/system_regions.json', './files/vehicle_types.json', './files/system_pricing_plan.json', './files/gbfs_versions.json', './files/system_calendar.json', './files/system_hours.json'];
+//const files: string[] = ['./files/station_information.json', './files/free_bike_status.json', './files/system_alerts.json', './files/system_regions.json', './files/vehicle_types.json', './files/system_pricing_plan.json', './files/gbfs_versions.json', './files/system_calendar.json', './files/system_hours.json'];
 // Main objects that are passed to the rdfVocabulary.ts.
 // there is one per json schema.
-var objects = ['gbfsvcb:Station', 'gbfsvcb:Bike', 'gbfsvcb:Alert', 'gbfsvcb:Region', 'gbfsvcb:VehicleType', 'gbfsvcb:PricingPlan', 'gbfsvcb:Version', 'gbfsvcb:Calendar', 'gbfsvcb:RentalHour'];
+//const objects: string[] = [ 'gbfsvcb:Station', 'gbfsvcb:Bike', 'gbfsvcb:Alert', 'gbfsvcb:Region', 'gbfsvcb:VehicleType', 'gbfsvcb:PricingPlan', 'gbfsvcb:Version', 'gbfsvcb:Calendar', 'gbfsvcb:RentalHour' ];
+var so = new Map();
+so.set('./files/station_information.json', 'gbfsvcb:Station');
+so.set('./files/free_bike_status.json', 'gbfsvcb:Bike');
+so.set('./files/system_alerts.json', 'gbfsvcb:Alert');
+so.set('./files/system_regions.json', 'gbfsvcb:Region');
+so.set('./files/vehicle_types.json', 'gbfsvcb:VehicleType');
+so.set('./files/system_pricing_plan.json', 'gbfsvcb:PricingPlan');
+so.set('./files/gbfs_versions.json', 'gbfsvcb:Version');
+so.set('./files/system_calendar.json', 'gbfsvcb:Calendar');
+so.set('./files/system_hours.json', 'gbfsvcb:RentalHour');
+console.log(so);
 var fs = require('fs');
 var hiddenClasses = [];
 var i = 0;
-for (var _i = 0, objects_1 = objects; _i < objects_1.length; _i++) {
-    var obj = objects_1[_i];
-    var config_1 = new Configuration_1.Configuration(files[i]);
+for (var _i = 0, _a = Array.from(so); _i < _a.length; _i++) {
+    var _b = _a[_i], key = _b[0], obj = _b[1];
+    console.log(obj);
+    // const config = new Configuration(files[i]);
+    var config = new Configuration_1.Configuration(key);
     i += 1;
-    var rdfVocab_1 = new rdfVocabulary_1.RDFVocabulary(config_1.getTermMapping(), config_1.getJsonSource(), obj);
-    console.log(config_1.getVocabURI());
-    rdfVocab_1.basicsToQuads();
-    hiddenClasses = rdfVocab_1.objectPropertiesToQuads(0);
+    var rdfVocab = new rdfVocabulary_1.RDFVocabulary(config.getTermMapping(), config.getJsonSource(), obj);
+    console.log(config.getVocabURI());
+    rdfVocab.basicsToQuads();
+    hiddenClasses = rdfVocab.objectPropertiesToQuads(0);
     // New classes might be have been added as range value for some properties. It is now time to explore those classes, 
     // e.g. "per_km_pricing" in system_pricing.json
-    for (var _a = 0, hiddenClasses_1 = hiddenClasses; _a < hiddenClasses_1.length; _a++) {
-        var cls = hiddenClasses_1[_a];
-        rdfVocab_1.setMainObject(cls);
-        rdfVocab_1.objectPropertiesToQuads(1);
+    for (var _c = 0, hiddenClasses_1 = hiddenClasses; _c < hiddenClasses_1.length; _c++) {
+        var cls = hiddenClasses_1[_c];
+        rdfVocab.setMainObject(cls);
+        rdfVocab.objectPropertiesToQuads(1);
     }
-    rdfVocab_1.writeTurtle();
-    rdfVocab_1.writeShacl();
+    rdfVocab.writeTurtle();
+    rdfVocab.writeShacl();
 }
 /**
  * Creating the json-ld context: hard-coded since common to all files. A static solution could be found.
  */
-var config = new Configuration_1.Configuration(files[0]);
-var rdfVocab = new rdfVocabulary_1.RDFVocabulary(config.getTermMapping(), config.getJsonSource(), objects[0]);
-var prefixes = rdfVocab.getPrefixes().prefixes;
-var mapping = config.getTermMapping();
+/*
+const config = new Configuration(files[0]);
+const rdfVocab = new RDFVocabulary(config.getTermMapping(), config.getJsonSource(), objects[0]);
+const prefixes:Map<string, string> = rdfVocab.getPrefixes().prefixes;
+const mapping:Map<string, string> = config.getTermMapping();
 //const size=  Object.keys(prefixes).length;
-var context = "{ \n \t \"@context\": { \n \t";
-for (var prefix in prefixes) {
+let context = `{ \n \t "@context": { \n \t`;
+for(const prefix in prefixes){
     //if( prefix != Array.from(Object.keys(prefixes))[size-1])
-    context += "\t \t \"" + prefix + "\": " + ("\"" + prefixes[prefix] + "\"") + ", \n";
-}
-;
-for (var _b = 0, _c = Array.from(mapping.entries()); _b < _c.length; _b++) {
-    var _d = _c[_b], key = _d[0], value = _d[1];
-    if (key != Array.from(mapping.keys()).pop()) {
-        context += "\t \t \"" + key + "\": " + ("\"" + value + "\"") + ", \n";
+    context += `\t \t "${prefix}": `+`"${prefixes[prefix]}"`+`, \n`;
+};
+for(const [key, value] of Array.from(mapping.entries())){
+    if (key != Array.from(mapping.keys()).pop()){
+        context += `\t \t "${key}": `+`"${value}"`+`, \n`;
     }
-    else {
-        context += "\t \t \"" + key + "\": " + ("\"" + value + "\"") + "\n";
+    else{
+        context += `\t \t "${key}": `+`"${value}"`+`\n`;
     }
-}
-;
-context += "\n  \t\u00A0} \n }";
+};
+context += `\n  \t } \n }`;
 // Write the Shacl shape on file
-fs.writeFileSync("build/ldContext.json", context, function (err) {
-    if (err) {
+fs.writeFileSync(`build/ldContext.json`, context , function(err){
+    if(err){
         return console.log("error");
     }
 });
+
+*/ 
--- main.ts
+++ main.ts
@@ -3,16 +3,32 @@ import {RDFVocabulary} from './rdfVocabulary';
 import {ShaclShape} from './shaclShape';
 
 //import {ShaclShape} from './shaclShape';
-const files: string[] = ['./files/station_information.json', './files/free_bike_status.json', './files/system_alerts.json', './files/system_regions.json', './files/vehicle_types.json', './files/system_pricing_plan.json', './files/gbfs_versions.json', './files/system_calendar.json', './files/system_hours.json'];
+//const files: string[] = ['./files/station_information.json', './files/free_bike_status.json', './files/system_alerts.json', './files/system_regions.json', './files/vehicle_types.json', './files/system_pricing_plan.json', './files/gbfs_versions.json', './files/system_calendar.json', './files/system_hours.json'];
 // Main objects that are passed to the rdfVocabulary.ts.
 // there is one per json schema.
-const objects: string[] = [ 'gbfsvcb:Station', 'gbfsvcb:Bike', 'gbfsvcb:Alert', 'gbfsvcb:Region', 'gbfsvcb:VehicleType', 'gbfsvcb:PricingPlan', 'gbfsvcb:Version', 'gbfsvcb:Calendar', 'gbfsvcb:RentalHour' ];
-const  fs = require('fs');
+//const objects: string[] = [ 'gbfsvcb:Station', 'gbfsvcb:Bike', 'gbfsvcb:Alert', 'gbfsvcb:Region', 'gbfsvcb:VehicleType', 'gbfsvcb:PricingPlan', 'gbfsvcb:Version', 'gbfsvcb:Calendar', 'gbfsvcb:RentalHour' ];
+
+let so = new Map<string, string>();
+so.set('./files/station_information.json', 'gbfsvcb:Station');
+so.set('./files/free_bike_status.json', 'gbfsvcb:Bike');
+so.set('./files/system_alerts.json', 'gbfsvcb:Alert');
+so.set('./files/system_regions.json', 'gbfsvcb:Region');
+so.set('./files/vehicle_types.json', 'gbfsvcb:VehicleType');
+so.set('./files/system_pricing_plan.json', 'gbfsvcb:PricingPlan');
+so.set('./files/gbfs_versions.json', 'gbfsvcb:Version');
+so.set('./files/system_calendar.json', 'gbfsvcb:Calendar');
+so.set('./files/system_hours.json', 'gbfsvcb:RentalHour');
+
+console.log(so);
+const fs = require('fs');
 
 let hiddenClasses = []
 let i = 0;
-for (const obj of objects){
-    const config = new Configuration(files[i]);
+for (let [key,obj] of Array.from(so)){
+    console.log(obj);
+   // const config = new Configuration(files[i]);
+    const config = new Configuration(key);
+
     i +=1;
     const rdfVocab = new RDFVocabulary(config.getTermMapping(), config.getJsonSource(), obj);
     console.log(config.getVocabURI());
@@ -31,9 +47,13 @@ for (const obj of objects){
     rdfVocab.writeTurtle();
     rdfVocab.writeShacl();
 }
+
+
 /**
  * Creating the json-ld context: hard-coded since common to all files. A static solution could be found.
  */
+
+/*
 const config = new Configuration(files[0]);
 const rdfVocab = new RDFVocabulary(config.getTermMapping(), config.getJsonSource(), objects[0]);
 const prefixes:Map<string, string> = rdfVocab.getPrefixes().prefixes;
@@ -59,3 +79,5 @@ fs.writeFileSync(`build/ldContext.json`, context , function(err){
         return console.log("error");
     }
 });
+
+*/
